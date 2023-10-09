5656f2a8a3b221d88b470ec7da1526bd8fa1499f
Julian Lefondeur <julian.lefondeur@gmail.com>
2023-10-09 11:45:10 +0200

    Added NgRx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app.routing-module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
