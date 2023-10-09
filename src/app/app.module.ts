5d62a63c9634f3dfa8c67173677beb9c9a4c9e48
Julian Lefondeur <julian.lefondeur@gmail.com>
2023-10-09 11:45:39 +0200

    Added cocktail search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { CocktailSearchComponent } from './components/cocktail-search/cocktail-search.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AppRoutingModule } from './app.routing-module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CocktailSearchComponent,
     HeaderComponent
   ],
   imports: [
---
+++ src/app/components/cocktail-search/cocktail-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cocktail-search',
+  templateUrl: './cocktail-search.component.html',
+  styleUrls: ['./cocktail-search.component.scss']
+})
+export class CocktailSearchComponent {
+
+}
